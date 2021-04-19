0c526b2a07373a5006403b4ee6bccb7405b8924d
Yidan Jin <yidan.jin@student.manchester.ac.uk>
2021-04-19 13:44:17 +0100

    Search for venues by name

--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueServiceImpl.java
@@ -3,12 +3,9 @@ package uk.ac.man.cs.eventlite.dao;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Service
