82e6e1ff8be049d3ffbe04b237a4e5f6f789ddc2
Karin Tange <karin.tange@student.manchester.ac.uk>
2021-04-26 04:03:19 +0100

    Add venue with road name and post code

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
@@ -66,25 +66,28 @@ public class VenuesController {
 		return "venues/index";
 	}
 
+	
+	
 	@GetMapping("/new")
 	public String newVenue(Model model) {
 		if(!model.containsAttribute("venues")) {
 			model.addAttribute("venues", new Venue());
 		}
-		
+		System.out.print("PRINT THIS");
 		return "venues/new";
 	}
-	
 	
 	@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public String createVenue(@RequestBody @Valid @ModelAttribute Venue venue, BindingResult errors,
 			Model model, RedirectAttributes redirectAttrs){
-		
+
 		if(errors.hasErrors()) {
+			System.out.print(errors);
 			model.addAttribute("venues", venue);
 			
 			return "venues/new";
 		}
+
 		
 		venueService.save(venue);
 		redirectAttrs.addFlashAttribute("ok_message", "New venue successfully added!");
@@ -92,49 +95,5 @@ public class VenuesController {
 		return "redirect:/venues";
 		
 	}
-	
-	/*
-	public void setLatitudeLongitude(Venue v) throws IOException {
-		String addr = v.getRoadName();
-		String post = v.getPostcode();
-		String accessToken = "pk.eyJ1IjoidzU3NjY0bWsiLCJhIjoiY2tuamhxa2c3MDE4ZjJwbDRjOXltd2FhMiJ9.3VK7Vq1Z6SClt1Ic2SzzQA";
-		String query = addr + " " + post;
-		MapboxGeocoding client = MapboxGeocoding.builder()
-				.accessToken(accessToken)
-				.query(query)
-				.mode(GeocodingCriteria.MODE_PLACES)
-				.limit(1) // limited to one search result to preserve API requests
-				.build();
-		
-		Response<GeocodingResponse> resp= client.executeCall();
-		GeocodingResponse gResponse = resp.body();
-		Point coords= gResponse.features().get(0).center();
-		v.setLatitude(coords.latitude());
-		v.setLongitude(coords.longitude());
-	}
- 	
-	@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public String createVenue(@RequestBody @Valid @ModelAttribute Venue venue, BindingResult errors,
-			Model model, RedirectAttributes redirectAttrs) {
-
-		if (errors.hasErrors()) {
-			model.addAttribute("venues", venue);
-			return "venues/new";
-		}
-		try {
-			setLatitudeLongitude(venue);
-		}
-		catch (IOException e){
-			e.printStackTrace();
-		}
-		venueService.save(venue);
-		redirectAttrs.addFlashAttribute("ok_message", "New Event added.");
-		
-		return "redirect:/venues";
-	}
-	*/
-
-
-
 	
 }
