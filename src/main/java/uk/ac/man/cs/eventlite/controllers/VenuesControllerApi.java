9a5a523270e388b93b3219c3cd0b98f5411c4992
Karin Tange <karin.tange@student.manchester.ac.uk>
2021-05-05 16:09:35 +0100

    API response for single venue

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
@@ -1,9 +1,12 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -16,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Venue;
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.response.VenueResponse;
 
 @RestController
 @RequestMapping(value = "/api/venues", produces = { MediaType.APPLICATION_JSON_VALUE, MediaTypes.HAL_JSON_VALUE })
@@ -31,22 +38,92 @@ public class VenuesControllerApi {
 	@Autowired
 	private VenueService venueService;
 	
+	@Autowired
+	private EventService eventService;
+	
 	@GetMapping
 	public CollectionModel<Venue> getAllVenues() {
-
+		
 		return venueCollection(venueService.findAll());
 	}
 	
-
 	private CollectionModel<Venue> venueCollection(Iterable<Venue> venues) {
 		Link selfLink = linkTo(methodOn(VenuesControllerApi.class).getAllVenues()).withSelfRel();
 
 		return CollectionModel.of(venues, selfLink);
 	}
-	
 
+//------Single Venue------------------------------------------------------------------------------------
+	
+	@GetMapping("{id}")
+	public VenueResponse venue(@PathVariable("id") long id) {
+		Venue theVenue = venueService.findOne(id);
+		
+		return detailedVenue(theVenue);
+	}
+	
+	private VenueResponse detailedVenue(Venue venue) {
+			
+		VenueResponse theVenue = new VenueResponse();
+		theVenue.name = venue.getName();
+		theVenue.capacity = venue.getCapacity();
+		
+		Link selfLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).withSelfRel();
+		Link venueLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).withRel("venue");
+		Link eventsLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).slash("events").withRel("events");
+		Link next3eventsLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).slash("next3events").withRel("next3events");
+		theVenue.add(selfLink, venueLink, eventsLink, next3eventsLink);
+		
+		return theVenue;
+		
+	}
+
+//------Next 3 Events------------------------------------------------------------------------------------
+	
+	@GetMapping("/{id}/next3events")
+	public CollectionModel<Event> nextThreeEvents(@PathVariable("id") long id){
+		
+		List<Event> events = eventService.findUpcomingEvents();
+		List<Event> upcomingEvents = new ArrayList<Event>();
+
+		while(upcomingEvents.size() < 3) {
+			for(Event e:events) {
+				if(e.getVenue().getId() == id) {
+					upcomingEvents.add(e);
+				}
+			}
+		}
+		
+		return eventCollection(upcomingEvents);
+	}
+
+//------Events Link------------------------------------------------------------------------------------
+
+	@GetMapping("/{id}/events")
+	public CollectionModel<Event> eventsAtVenue(@PathVariable("id") long id){
+		
+		List<Event> events = eventService.findUpcomingEvents();
+		List<Event> upcomingEvents = new ArrayList<Event>();
+
+		for(Event e:events) {
+			if(e.getVenue().getId() == id) {
+				upcomingEvents.add(e);
+			}
+		}
+
+		return eventCollection(upcomingEvents);
+	}
+
+	private CollectionModel<Event> eventCollection(Iterable<Event> events) {
+		Link selfLink = linkTo(methodOn(EventsControllerApi.class).getAllEvents()).withSelfRel();
+
+		return CollectionModel.of(events, selfLink);
+	}
+//------------------------------------------------------------------------------------------------------	
+
+	
 	@GetMapping("/new")
-	public ResponseEntity<?> newEvent(){
+	public ResponseEntity<?> newVenue(){
 		return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
 	}
 	
@@ -61,4 +138,7 @@ public class VenuesControllerApi {
 		
 		return ResponseEntity.created(location).build();
 	}
+				
+	
+		
 }
